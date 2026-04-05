c03ca56b1a42c952b9b58f4cc6f353052135d0bc
Cole Harten <charten@college.harvard.edu>
2026-04-05 15:51:25 -0400

    rm build

--- build/CMakeFiles/cppcoro_static_thread_pool.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libcppcoro_static_thread_pool.a"
-)
--- build/CMakeFiles/cppcoro_static_thread_pool.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for cppcoro_static_thread_pool.
